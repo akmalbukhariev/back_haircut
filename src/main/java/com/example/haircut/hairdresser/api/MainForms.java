daf706c6c2bd6ca762be88d84914e36d38ea2b59
Buxariyev Akmal <akmalbukhariev@gmail.com>
2023-12-08 11:27:17 +0900

    Created the backend for haircut app.

---
+++ src/main/java/com/example/haircut/hairdresser/api/MainForms.java
@@ -0,0 +1,7 @@
+package com.example.haircut.hairdresser.api;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainForms {
+}
---
+++ src/test/java/com/example/haircut/HaircutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.haircut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaircutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
